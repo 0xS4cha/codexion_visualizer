d6780e6b51ce13286d34dec9470bcc42cb087733
Sacha <servantonsacha@gmail.com>
2026-05-12 23:25:40 +0200

    fix/login

--- api/login.ts
+++ api/login.ts
@@ -1,11 +1,11 @@
 ﻿import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const params = new URLSearchParams({
-    client_id: process.env.FORTY_TWO_CLIENT_ID || "",
-    redirect_uri: process.env.FORTY_TWO_REDIRECT_URI || "",
+    client_id: (process.env.FORTY_TWO_CLIENT_ID || "") as string,
+    redirect_uri: (process.env.FORTY_TWO_REDIRECT_URI || "") as string,
     response_type: "code",
     scope: "public",
-  });
+  } as Record<string, string>);
   console.log("CLIENT_ID:", process.env.FORTY_TWO_CLIENT_ID);
   console.log("REDIRECT_URI:", process.env.FORTY_TWO_REDIRECT_URI);
   const authUrl = `https://api.intra.42.fr/oauth/authorize?${params}`;
